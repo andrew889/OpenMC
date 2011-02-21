d1d682ef1e4c6b76906ec601143904fa3e71ada8
yuriks <yuriks.br@gmail.com>
2011-02-21 17:06:40 -0300

    Client main now opens an empty window that does nothing at all. Weee.

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,4 +1,20 @@
+#include <SFML/Window.hpp>
+
 int main(int argc, char** argv)
 {
+	sf::Window app(sf::VideoMode(800, 600, 32), "OpenMC");
+
+	bool run = true;
+	while (run) {
+		sf::Event ev;
+		while (app.GetEvent(ev)) {
+			if (ev.Type == sf::Event::Closed) {
+				run = false;
+			}
+		}
+
+		app.Display();
+	}
+
 	return 0;
 }
